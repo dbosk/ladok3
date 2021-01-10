37c211f8ea4ab005c232402601ec62d225f3de4e
Daniel Bosk <dbosk@kth.se>
2021-01-10 23:44:40 +0100

    WIP on grading

--- ladok_lib/example_Student.py
+++ ladok_lib/example_Student.py
@@ -1,12 +1,30 @@
 import ladok3
 
 ladok = ladok3.LadokSessionKTH(
-        "dbosk", "my not-secret password",
+        "dbosk", "some password",
         test_environment=True) # for experiments
 
 me = ladok.get_student("8506097891")
+me2 = ladok.get_student("de709f81-a867-11e7-8dbf-78e86dc2470c")
 
 print(f"{me.personnummer} {me.last_name}, {me.first_name}")
+print(f"{me2.personnummer} {me2.last_name}, {me2.first_name}")
+print(f"{me.ladok_id} == {me2.ladok_id}")
+print()
 
-for course in me.courses:
-    print(course)
+student = ladok.get_student("1234561234")
+print(f"{student.personnummer} {student.first_name} {student.last_name}")
+for course in student.courses(code="DD1315"):
+    print(f"round_id = {course.round_id}")
+    print(f"education_id = {course.education_id}")
+    print(f"instance_id = {course.instance_id}")
+    print(f"code = {course.code}")
+    print(f"name = {course.name}")
+    print("Results:")
+    for result in course.results:
+        result.set_grade("P", "2021-01-01")
+        print(f"component_id = {result.component_id}")
+        print(f"education_id = {result.education_id}")
+        print(f"grade = {result.grade} ({result.date})")
+        print(f"attested = {result.attested}")
+        print(result.ladok_json.keys())
