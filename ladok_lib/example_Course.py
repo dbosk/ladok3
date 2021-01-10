047c6ca815027eaa2919d6571d372956a34fb11a
Daniel Bosk <dbosk@kth.se>
2021-01-10 23:44:40 +0100

    WIP on grading

--- ladok_lib/example_Course.py
+++ ladok_lib/example_Course.py
@@ -1,7 +1,16 @@
 import ladok3
 
 ladok = ladok3.LadokSessionKTH(
-        "dbosk", "my not-secret password",
+        "dbosk", "some password",
         test_environment=True) # for experiments
 
-prgi = ladok.get_course("DD1315")
+#prgi = ladok.get_course("DD1315")
+
+instance = ladok3.CourseInstance(
+        ladok=ladok,
+        **ladok.course_instances_JSON("DD1315")["Resultat"][0])
+print(instance.ladok_json["Utbildningsinstans"]["Uid"])
+print(instance.ladok_json["Utbildningsinstans"]["UtbildningUID"])
+print(instance.ladok_json["Utbildningsinstans"]["Utbildningskod"])
+print(instance.instance_code)
+print(instance.ladok_json["Utbildningsinstans"].keys())
