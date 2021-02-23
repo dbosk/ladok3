f82526e26ceacaf78ab2be3b27d1cae209cb0e75
Daniel Bosk <dbosk@kth.se>
2021-02-23 16:26:03 +0100

    Adds CourseRound

--- ladok_lib/example_Course.py
+++ ladok_lib/example_Course.py
@@ -2,16 +2,10 @@ import ladok3
 import os
 
 ladok = ladok3.LadokSessionKTH(
-        os.environ["KTH_LOGIN", os.environ["KTH_PASSWD"],
+        os.environ["KTH_LOGIN"], os.environ["KTH_PASSWD"],
         test_environment=True) # for experiments
 
-#prgi = ladok.get_course("DD1315")
+prgiX = ladok.get_course_rounds(code="DD1315")
 
-instance = ladok3.CourseInstance(
-        ladok=ladok,
-        **ladok.course_instances_JSON("DD1315")["Resultat"][0])
-print(instance.ladok_json["Utbildningsinstans"]["Uid"])
-print(instance.ladok_json["Utbildningsinstans"]["UtbildningUID"])
-print(instance.ladok_json["Utbildningsinstans"]["Utbildningskod"])
-print(instance.instance_code)
-print(instance.ladok_json["Utbildningsinstans"].keys())
+for prgi in prgiX:
+    print(f"{prgi.start}--{prgi.end}")
