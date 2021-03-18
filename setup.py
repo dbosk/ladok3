0357f2510afc4b8ab7f1d547f62125f1a267b903
Daniel Bosk <dbosk@kth.se>
2021-03-18 10:02:48 +0100

    Bumps version to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ladok3",
-    version = "1.0",
+    version = "1.1",
     author = "Daniel Bosk, Alexander Baltatzis, Gerald Q. Maguire Jr",
     author_email = "dbosk@kth.se",
     description = "Python wrapper for the LADOK3 REST API",
