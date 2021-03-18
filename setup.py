735972c1c85e57751f35825122af08289fdc9683
Daniel Bosk <dbosk@kth.se>
2021-03-18 10:54:53 +0100

    Fixes versions of dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ladok3",
-    version = "1.2",
+    version = "1.3",
     author = "Daniel Bosk, Alexander Baltatzis, Gerald Q. Maguire Jr",
     author_email = "dbosk@kth.se",
     description = "Python wrapper for the LADOK3 REST API",
@@ -36,10 +36,10 @@ setup(
     python_requires = ">=3.8",
     install_requires = [
         "appdirs>=1.4.4",
-        "argcomplete>=1.12.2",
-        "cachetools>=4.2.1"
-        "requests >= 2.24.0",
-        "urllib3 >= 1.25.8"
+        "argcomplete>=1.12.0",
+        "cachetools>=4.0.0"
+        "requests>=2.24.0",
+        "urllib3>=1.25.8"
     ]
 )
 
