b7315753713925eb4d9a28d1fde11beb1e3b803b
Daniel Bosk <dbosk@kth.se>
2021-04-14 12:12:50 +0200

    Adds bulk reporting through CSV on stdin

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ladok3",
-    version = "1.8",
+    version = "1.9",
     author = "Daniel Bosk, Alexander Baltatzis, Gerald Q. Maguire Jr",
     author_email = "dbosk@kth.se",
     description = "Python wrapper for the LADOK3 REST API",
